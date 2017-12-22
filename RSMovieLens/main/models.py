c7b6021ed440bfe93db2ccc48cec4ceb80f45110
Javier Parra <javierparraoyola619@hotmail.com>
2017-12-22 08:47:34 +0100

    Modelos

--- RSMovieLens/main/models.py
+++ RSMovieLens/main/models.py
@@ -1,41 +1,29 @@
 from django.db import models
 from django.core.validators import MinValueValidator,MaxValueValidator,URLValidator
 
+class Usuario(models.Model):
+    idUsuario = models.IntegerField(unique=True)
+    edad = models.IntegerField(min_value = 0, max_value=200)
+    localizacion = models.CharField()
 
-class Occupation(models.Model):
-    occupationName = models.CharField(max_length=30)
     def __unicode__(self):
-        return unicode(self.occupationName)
- 
-    
-class Genre(models.Model):
-    genreName = models.CharField(max_length=20) 
-    def __unicode__(self):
-        return unicode(self.genreName)    
-    
-      
-class UserInformation(models.Model):
-    age = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
-    gender = models.CharField(max_length=1, choices=(('F', 'Female'),('M','Male'),))
-    occupation = models.ForeignKey(Occupation, on_delete=models.DO_NOTHING)
-    zipCode = models.CharField(max_length=8)
-    def __unicode__(self):
-        return unicode(self.gender+self.zipCode)
-  
-    
-class Film(models.Model):
-    movieTitle = models.CharField(max_length=100)
-    releaseDate = models.DateField(null=True, blank=True)
-    releaseVideoDate = models.DateField(null=True, blank=True)
-    IMDbURL = models.URLField(validators=[URLValidator()])
-    genres = models.ManyToManyField(Genre)
-    ratings = models.ManyToManyField(UserInformation, through="Rating")
-    def __unicode__(self):
-        return unicode(self.movieTitle)
+        return self.idUsuario
 
-    
-class Rating(models.Model):
-    user = models.ForeignKey(UserInformation)
-    film = models.ForeignKey(Film)
-    rateDate = models.DateField(null=True, blank=True)
-    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+class Libro(models.Model):
+    isbn = models.CharField()
+    titulo = models.CharField()
+    autor = models.CharField()
+    añoPublicacion = models.IntegerField
+    editor = models.CharField()
+    url = models.CharField()
+
+    def __unicode__(self):
+        return self.isbn
+
+class Puntuacion(models.Model):
+    idUsuario = models.ForeignKey(Usuario.idUsuario)
+    isbn = models.ForeignKey(Libro.isbn)
+    puntuacion = models.IntegerField(min_value=1, max_value=10)
+
+    def __unicode__(self):
+        return self.idUsuario + self.isbn
